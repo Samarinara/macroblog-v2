c2954f94e715f1005e317da1108d4f2e4cd6b69c
samarinara <samkatevatis@gmail.com>
2025-09-26 16:20:24 -0600

    fixed responsive text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { agent } from "@/lib/api";
+import "./page.css"
 
 export default async function Homepage() {
 
@@ -7,11 +8,14 @@ export default async function Homepage() {
       <div className="grid grid-cols-2">
         <div className="bg-black text-white flex relative">
           <div className="absolute md:bottom-0 right-[5vw] bottom-[2vh] text-[10vw]">
-            <span>M</span>
-            <span>A</span>
-            <span>C</span>
-            <span>R</span>
-            <span>O</span>
+            <div className="flex flex-col md:flex-row space-x-0 -py-25">
+              <span className=" dynamic-column-text">M</span>
+              <span className=" dynamic-column-text">A</span>
+              <span className=" dynamic-column-text">C</span>
+              <span className=" dynamic-column-text">R</span>
+              <span className=" dynamic-column-text">O</span>
+
+            </div>
           </div>
         </div>
         <div></div>
@@ -21,10 +25,10 @@ export default async function Homepage() {
         <div className="bg-black text-white relative">
           <div className="absolute md:top-0 left-[5vw] top-[2vh] text-[10vw]">
             <div className="flex flex-col md:flex-row">
-              <span>B</span>
-              <span>L</span>
-              <span>O</span>
-              <span>G</span>
+              <span className=" dynamic-column-text">B</span>
+              <span className=" dynamic-column-text">L</span>
+              <span className=" dynamic-column-text">O</span>
+              <span className=" dynamic-column-text">G</span>
             </div>
           </div>
         </div>
