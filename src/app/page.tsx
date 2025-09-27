0400e632c5fa510870b682edf45eccb245f9445d
samarinara <samkatevatis@gmail.com>
2025-09-27 17:23:05 -0600

    responsive search box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,45 +2,24 @@ import { agent } from "@/lib/api";
 import "./page.css"
 import { Search } from "@mui/icons-material"
 
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+
 export default async function Homepage() {
 
   return (
-    <div className="grid grid-rows-{2} w-screen h-screen">
-      <div className="grid grid-cols-2">
-        <div className="bg-black text-white flex relative">
-          <div className="absolute md:bottom-0 right-[5vw] bottom-[2vh] text-[10vw]">
-            <div className="flex flex-col md:flex-row space-x-0 -py-25">
-              <span className=" dynamic-column-text">M</span>
-              <span className=" dynamic-column-text">A</span>
-              <span className=" dynamic-column-text">C</span>
-              <span className=" dynamic-column-text">R</span>
-              <span className=" dynamic-column-text">O</span>
-
-            </div>
-          </div>
+    <div className="grid lg:grid-cols-2 w-screen h-screen bg-gray-100">
+      <div className="flex bg-green-900 allign-center justify-center items-center lg:text-[7vw] text-[15vw] text-white flex-col lg:h-[100vh] h-[33vh] p-3">
+        <h1>MACROBLOG</h1>
+        <div className="flex flex-row gap-4">
+          <Input placeholder="Search" className="lg:w-[30vw] lg:h-[7vh] h-[5vh] w-[60vw] text-xl border"/>
+          <Button className="h-[5vh] w-[20vw] lg:w-[5vw] lg:h-[7vh] "><Search /></Button>
         </div>
-        <div></div>
       </div>
-      <div className="grid grid-cols-2">
-        <div className="relative">
-          <div className="absolute right-[2vw] top-[2vw]">
-            <div className="bg-black w-[10vh] h-[10vh] text-[9vh] rounded-full text-white flex items-center justify-center ">
-              <Search style={{ width: "7vh", height: "7vh"}} className="w-[10vh] h-[10vh] fill-current"></Search>
-            </div>
-          </div>
-        </div>
-        <div className="bg-black text-white relative">
-          <div className="absolute md:top-0 left-[5vw] top-[2vh] text-[15vw]">
-            <div className="flex flex-col md:flex-row">
-              <span className=" dynamic-column-text">B</span>
-              <span className=" dynamic-column-text">L</span>
-              <span className=" dynamic-column-text">O</span>
-              <span className=" dynamic-column-text">G</span>
-            </div>
-          </div>
-        </div>
+      <div>
+
       </div>
     </div>
   );
 }
-
