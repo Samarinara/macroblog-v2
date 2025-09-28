2d3d12dd77a6538dc5aa313578bb7b81115e10c2
samarinara <samkatevatis@gmail.com>
2025-09-27 20:11:40 -0600

    improved responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,15 +16,15 @@ export default async function Homepage() {
 
   return (
     <div className="grid lg:grid-cols-2 w-screen h-screen bg-gray-100 overflow-x-hidden">
-      <div className="flex bg-green-900 allign-center justify-center items-center lg:text-[7vw] text-[15vw] text-white flex-col lg:h-[100vh] h-[25vh] p-3">
+      <div className="flex bg-green-900 allign-center justify-center items-center lg:text-[7vw] text-[15vw] text-white flex-col lg:h-[100vh] md:h-[40vh] h-[25vh] p-3">
         <h1>MACROBLOG</h1>
         <div className="flex flex-row gap-4">
           <Input placeholder="Search" className="lg:w-[30vw] lg:h-[7vh] h-[5vh] w-[60vw] text-xl border"/>
           <Button className="h-[5vh] w-[20vw] lg:w-[5vw] lg:h-[7vh] "><Search /></Button>
         </div>
       </div>
-      <div className="flex overflow-auto m-[1vw] lg:allign-center lg:justify-center flex-col sm:flex-row sm:w-[50vw] w-[100vw] overflow-x-hidden">
-        <Card className="lg:fixed bg-green-900 text-white p-4 m-[1vh] text-[3vh] lg:z-1">
+      <div className="flex m-[1vw] lg:allign-center lg:justify-center flex-col sm:flex-row md:w-[50vw] w-[100vw] lg:overflow-x-hidden md:overflow-x-visible sm:overflow-x-hidden">
+        <Card className="lg:fixed bg-green-900 text-white p-4 m-[1vh] text-[3vh] lg:z-1 md:h-[15vh] lg:h-auto sm:h-[12vh]">
           <h1>Featured Blogs</h1>
         </Card>
         <ul>
