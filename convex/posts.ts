7fb7e075b8c6db30e9061e0208fa9d118c66268e
samarinara <samkatevatis@gmail.com>
2025-09-30 15:13:27 -0600

    removed pesky doc

--- convex/posts.ts
+++ convex/posts.ts
@@ -1,7 +1,6 @@
 import { mutation, query, action } from "./_generated/server";
 import { v } from "convex/values";
 import { api } from "./_generated/api";
-import { type Doc } from "./_generated/dataModel";
 import type { Id } from "./_generated/dataModel";
 // Create a new post
 export const createPost = mutation({
